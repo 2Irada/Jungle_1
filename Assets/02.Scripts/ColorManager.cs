621b320b0aaac25c32189dfd27e92b9116627fea
최민준 <kai77727772@gmail.com>
2023-08-16 16:55:18 +0900

    dotted line

--- Assets/02.Scripts/ColorManager.cs
+++ Assets/02.Scripts/ColorManager.cs
@@ -11,8 +11,12 @@ public class ColorManager : MonoBehaviour
     [Header("Colors")]
     public Color black;
     public Color red;
-    public Color yellow;
     public Color green;
+    public Color blue;
+    public Color blackHighlight;
+    public Color redHighlight;
+    public Color greenHighlight;
+    public Color blueHighlight;
 
     public delegate void MainColoringChanged();
     public MainColoringChanged mainColoringChanged;
@@ -21,6 +25,11 @@ public class ColorManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
+    }
+
+    private void Start()
+    {
+        SwitchMainColoring(Coloring.Red);
     }
 
     private void Update()
@@ -50,9 +59,9 @@ public class ColorManager : MonoBehaviour
     public void AutoSwitchMainColoring()
     {
         if (mainColoring == Coloring.Black) { SwitchMainColoring(Coloring.Red); }
-        else if (mainColoring == Coloring.Red) { SwitchMainColoring(Coloring.Yellow); }
-        else if (mainColoring == Coloring.Yellow) { SwitchMainColoring(Coloring.Green); }
-        else if (mainColoring == Coloring.Green) { SwitchMainColoring(Coloring.Red); }
+        else if (mainColoring == Coloring.Red) { SwitchMainColoring(Coloring.Green); }
+        else if (mainColoring == Coloring.Green) { SwitchMainColoring(Coloring.Blue); }
+        else if (mainColoring == Coloring.Blue) { SwitchMainColoring(Coloring.Red); }
     }
 
     /// <summary>
@@ -68,10 +77,28 @@ public class ColorManager : MonoBehaviour
                 return black;
             case Coloring.Red:
                 return red;
-            case Coloring.Yellow:
-                return yellow;
             case Coloring.Green:
                 return green;
+            case Coloring.Blue:
+                return blue;
+            default:
+                Debug.LogError("Coloring out of bounds.");
+                return black;
+        }
+    }
+
+    public Color GetHighlightColorByColoring(Coloring coloring)
+    {
+        switch (coloring)
+        {
+            case Coloring.Black:
+                return blackHighlight;
+            case Coloring.Red:
+                return redHighlight;
+            case Coloring.Green:
+                return greenHighlight;
+            case Coloring.Blue:
+                return blueHighlight;
             default:
                 Debug.LogError("Coloring out of bounds.");
                 return black;
@@ -83,6 +110,6 @@ public enum Coloring
 {
     Black,
     Red,
-    Yellow,
-    Green
+    Green,
+    Blue
 }
