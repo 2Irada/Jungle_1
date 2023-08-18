d3fec2ec6c48948298ea39b70f43a4bd284cc86f
최민준 <kai77727772@gmail.com>
2023-08-18 14:03:00 +0900

    빌드 오류 수정

--- Assets/02.Scripts/CameraControllerEditor.cs
+++ Assets/02.Scripts/CameraControllerEditor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+#if UNITY_EDITOR
 [CustomEditor(typeof(CameraController))]
 public class CameraControllerEditor : Editor
 {
@@ -21,3 +22,4 @@ public class CameraControllerEditor : Editor
         }
     }
 }
+#endif
